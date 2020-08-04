6b919290d8b6c727b0f3baacc488eba26965efe5
yhanli <yhanl@hotmail.com>
2020-08-05 08:38:09 +1200

    add bash command

---
+++ pull.sh
@@ -0,0 +1,2 @@
+cp -avR media_root_dist/ media_root/
+cp -av db.sqlite3_dist db.sqlite3
---
+++ push.sh
@@ -0,0 +1,5 @@
+cp -avR media_root/ media_root_dist
+cp -av db.sqlite3 db.sqlite3_dist
+git add .
+git commit -m "dev fast push"
+git push
