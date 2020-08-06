2b2f87ab075061248878852efacef70388988985
Yhanli <45836848+Yhanli@users.noreply.github.com>
2020-08-06 12:30:32 +1200

    dev fast push

--- pull.sh
+++ pull.sh
@@ -1,8 +1,8 @@
-cp -avR media_root_dist/ media_root/
-cp -av db.sqlite3_dist db.sqlite3
-npm run build
+sudo cp -avR media_root_dist/ media_root/
+sudo cp -av db.sqlite3_dist db.sqlite3
+sudo npm run build
 pipenv run python manage.py makemigrations
 pipenv run python manage.py migrate
 pipenv run python manage.py collectstatic
-cp -avR media_root_dist/ media_root/
-cp -av db.sqlite3_dist db.sqlite3
+sudo cp -avR media_root_dist/ media_root/
+sudo cp -av db.sqlite3_dist db.sqlite3
