70920b485d13ddaec2d6c872059da0f1b82aeec0
yhanli <yhanl@hotmail.com>
2020-08-06 16:36:45 +1200

    dev fast push

--- frontend/src/components/pages/aboutus/story.js
+++ frontend/src/components/pages/aboutus/story.js
@@ -19,7 +19,7 @@ class Story extends Component {
     }
 
     nextSlide = () => {
-        const element = document.getElementById("content-section");
+        const element = document.getElementById("content-section-flex");
         smoothscroll.polyfill();
         window.scroll({
             top:element.offsetTop - 50,
@@ -47,58 +47,89 @@ class Story extends Component {
                                     </div>
                                 </div>
                                 <div>
-                                    <div className="content-section" id="content-section" style={{
-                                        backgroundImage: `url(${data.story.light_background_image})`,
-                                        height: '100%',
-                                        backgroundPosition: 'center',
-                                        backgroundRepeat: 'no-repeat',
-                                        backgroundSize: 'cover',
-                                        // paddingTop: "3em"
-                                    }}>
-                                        {/*<div><br/></div>*/}
-                                        <div id={`subsection1`} className="subsection">
-
-                                            <div className="subsection-text">
-                                                <h2 className="first-text">{data.section1_title}</h2>
-                                                <p>{data.section1_paragraph}</p>
-                                                <p className="famous-quote"><span className="quote">{data.famous_quote.split("–")[0]}</span> - <span className="quote-by">{data.famous_quote.split("–")[1]}</span></p>
+                                    <div className="content-section-flex" id="content-section-flex">
+                                        <div className="subsection-flex">
+                                            <div className="subsection-head subsection-text">
+                                                <h2>{data.story.main_section_heading}</h2>
+                                                <p>{data.story.main_section_paragraph}</p>
                                             </div>
-                                            <div className="row-column">
-                                                <div className="subsection-text">
-                                                    <h2>{data.section2_title}</h2>
-                                                    <p>{data.section2_paragraph}</p>
+                                            <div className="subsection-body">
+                                                <div>
+                                                    <img src={data.story.section1_image}/>
                                                 </div>
-                                                <div></div>
-                                                {/*<div></div>*/}
-
-                                            </div>
-
-                                            <div className="row-column">
-                                                {/*<div></div>*/}
-                                                <div></div>
-                                                <div className="subsection-text">
-                                                    <h2>{data.section3_title}</h2>
-                                                    <p>{data.section3_paragraph}</p>
+                                                <div className="subsection-head subsection-text">
+                                                    <h2>{data.story.section1_heading_1}</h2>
+                                                    <p>{data.story.section1_paragraph_1}</p>
                                                 </div>
                                             </div>
-                                            <div className="row-column" style={{
-                                                paddingBottom:"5vw"
-                                            }}>
+                                            <div className="subsection-body2">
                                                 <div className="subsection-text">
-                                                    <h2>{data.section4_title}</h2>
-                                                    {data.section4_paragraph ? <p>{data.section4_paragraph}</p>:""}
-                                                    <img src={data.section4_image}
-                                                         className="aboutus-img" alt="inspire"/>
-                                                </div>
-                                                <div></div>
-                                                <div></div>
-                                            </div>
+                                                    <blockquote>
+                                                        <p>
+                                                            {data.story.section1_subparagraph_1}
+                                                        </p>
+                                                    </blockquote>
 
+                                                </div>
+                                            </div>
+                                            <div className="subsection-tail">
+                                                <div className="subsection-text text-bg">
+                                                    <p>{data.story.section1_subparagraph_2}</p>
+                                                </div>
+                                                <div className="subsection-text">
+                                                    <p>{data.story.section1_subparagraph_3}</p>
+                                                </div>
+                                            </div>
+                                            <div className="subsection-end">
+                                                <div className="subsection-text">
+                                                    <p className="famous-quote"><span className="quote">{data.story.section1_quote_1.split("–")[0]}</span> <span className="quote-by">- {data.story.section1_quote_1.split("–")[1]}</span></p>
+                                                    {/*<p>{data.story.section1_quote_1}</p>*/}
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="subsection-divider-line"></div>
+
+                                        <div className="subsection-flex">
+                                            {/*<div className="subsection-head subsection-text">*/}
+                                            {/*    <h2>{data.story.section2_heading_1}</h2>*/}
+                                            {/*    <p>{data.story.section2_paragraph_1}</p>*/}
+                                            {/*</div>*/}
+                                            <div className="subsection-body">
+                                                <div>
+                                                    <img src={data.story.section2_image}/>
+                                                </div>
+                                                <div className="subsection-head subsection-text">
+                                                    <h2>{data.story.section2_heading_1}</h2>
+                                                    <p>{data.story.section2_paragraph_1}</p>
+                                                </div>
+                                            </div>
+                                            <div className="subsection-body2">
+                                                <div className="subsection-text text-bg">
+                                                    {/*<blockquote>*/}
+                                                        <p>
+                                                            {data.story.section2_subparagraph_1}
+                                                        </p>
+                                                    {/*</blockquote>*/}
+
+                                                </div>
+                                            </div>
+                                            <div className="subsection-tail">
+                                                <div className="subsection-text">
+                                                    <p>{data.story.section2_subparagraph_2}</p>
+                                                </div>
+                                                <div className="subsection-text text-bg">
+                                                    <p>{data.story.section2_subparagraph_3}</p>
+                                                </div>
+                                            </div>
+                                            <div className="subsection-end">
+                                                <div className="subsection-text">
+                                                    <p className="famous-quote"><span className="quote">{data.story.section2_quote_1.split("–")[0]}</span> <span className="quote-by">- {data.story.section2_quote_1.split("–")[1]}</span></p>
+                                                    {/*<p>{data.story.section1_quote_1}</p>*/}
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div>
-                                    <br />
                                 </div>
                             </div>
                         </Fragment>
