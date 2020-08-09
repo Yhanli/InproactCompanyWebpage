f274453dc7090a5fe3b1f4af8ad2a8606f19bc9a
yhanli <yhanl@hotmail.com>
2020-08-09 13:18:24 +1200

    dev fast push

--- pull.sh
+++ pull.sh
@@ -7,5 +7,3 @@ sudo npm run build
 pipenv run python manage.py makemigrations
 pipenv run python manage.py migrate
 pipenv run python manage.py collectstatic
-sudo cp -avR media_root_dist/ media_root/
-sudo cp -av db.sqlite3_dist db.sqlite3
