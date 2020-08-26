546481aac9ce80e323a370b7cf3ab9a5e95f29f5
yhanli <leeyuhan@qq.com>
2020-08-27 11:29:07 +1200

    remove pipenv sync, it is likely to cause significant impact to low capacity EC

--- pull.sh
+++ pull.sh
@@ -8,7 +8,6 @@ sudo cp -R media_root_dist/ media_root/
 sudo cp db.sqlite3_dist db.sqlite3
 sudo chmod -R 777 *
 sudo npm run build
-pipenv sync
 pipenv run python manage.py makemigrations
 pipenv run python manage.py migrate
 pipenv run python manage.py collectstatic
